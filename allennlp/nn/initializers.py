46997de0fdaae1b8b222bf891b3ef6fe893656b4
teffland <tom.effland@gmail.com>
2018-05-08 18:58:00 -0400

    elmo works with pytorch 0.4

--- allennlp/nn/initializers.py
+++ allennlp/nn/initializers.py
@@ -1,4 +1,9 @@
 """
+NOTE:
+----
+EDITED TO NOT MAKE RECURSIVE VARIABLE INIT CALLS SINCE pytorch==0.4 merged Tensor and Variable
+----
+
 An initializer is just a PyTorch function.
 Here we implement a proxy class that allows us
 to register them and supply any additional function arguments
@@ -88,9 +93,9 @@ def uniform_unit_scaling(tensor: torch.Tensor, nonlinearity: str = "linear"):
     -------
     The initialised tensor.
     """
-    if isinstance(tensor, Variable):
-        uniform_unit_scaling(tensor.data, nonlinearity)
-        return tensor
+    #if isinstance(tensor, Variable):
+    #    uniform_unit_scaling(tensor.data, nonlinearity)
+    #    return tensor
 
     size = 1.
     # Estimate the input size. This won't work perfectly,
@@ -129,28 +134,28 @@ def block_orthogonal(tensor: torch.Tensor,
         The gain (scaling) applied to the orthogonal initialization.
     """
 
-    if isinstance(tensor, Variable):
-        block_orthogonal(tensor.data, split_sizes, gain)
-    else:
-        sizes = list(tensor.size())
-        if any([a % b != 0 for a, b in zip(sizes, split_sizes)]):
-            raise ConfigurationError("tensor dimensions must be divisible by their respective "
-                                     "split_sizes. Found size: {} and split_sizes: {}".format(sizes, split_sizes))
-        indexes = [list(range(0, max_size, split))
-                   for max_size, split in zip(sizes, split_sizes)]
-        # Iterate over all possible blocks within the tensor.
-        for block_start_indices in itertools.product(*indexes):
-            # A list of tuples containing the index to start at for this block
-            # and the appropriate step size (i.e split_size[i] for dimension i).
-            index_and_step_tuples = zip(block_start_indices, split_sizes)
-            # This is a tuple of slices corresponding to:
-            # tensor[index: index + step_size, ...]. This is
-            # required because we could have an arbitrary number
-            # of dimensions. The actual slices we need are the
-            # start_index: start_index + step for each dimension in the tensor.
-            block_slice = tuple([slice(start_index, start_index + step)
-                                 for start_index, step in index_and_step_tuples])
-            tensor[block_slice] = torch.nn.init.orthogonal(tensor[block_slice].contiguous(), gain=gain)
+    #if isinstance(tensor, Variable):
+    #    block_orthogonal(tensor.data, split_sizes, gain)
+    #else:
+    sizes = list(tensor.size())
+    if any([a % b != 0 for a, b in zip(sizes, split_sizes)]):
+        raise ConfigurationError("tensor dimensions must be divisible by their respective "
+                                 "split_sizes. Found size: {} and split_sizes: {}".format(sizes, split_sizes))
+    indexes = [list(range(0, max_size, split))
+               for max_size, split in zip(sizes, split_sizes)]
+    # Iterate over all possible blocks within the tensor.
+    for block_start_indices in itertools.product(*indexes):
+        # A list of tuples containing the index to start at for this block
+        # and the appropriate step size (i.e split_size[i] for dimension i).
+        index_and_step_tuples = zip(block_start_indices, split_sizes)
+        # This is a tuple of slices corresponding to:
+        # tensor[index: index + step_size, ...]. This is
+        # required because we could have an arbitrary number
+        # of dimensions. The actual slices we need are the
+        # start_index: start_index + step for each dimension in the tensor.
+        block_slice = tuple([slice(start_index, start_index + step)
+                             for start_index, step in index_and_step_tuples])
+        tensor[block_slice] = torch.nn.init.orthogonal(tensor[block_slice].contiguous(), gain=gain)
 
 
 def _initializer_wrapper(init_function: Callable[..., None]) -> Type[Initializer]:
--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -4,6 +4,8 @@ pip install -r requirements.txt
 python -m nltk.downloader punkt
 spacy download en_core_web_sm
 
+pip install git+https://github.com/lanpa/tensorboard-pytorch
+
 # only install test requirements if explicitly specified
 if [[ "$INSTALL_TEST_REQUIREMENTS" == "true" ]]; then
     pip install -r requirements_test.txt
